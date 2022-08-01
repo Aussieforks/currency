457c1ea9b6784f582fa091714662304a9860deb1
Blockhead <jbis1337@hotmail.com>
2022-08-01 13:18:03 +1000

    Update settings, README and locale
    Settingtypes: change some settings, add barter chest setting
    
    Expand the README and fix locale files

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-currency
-========
+# Currency Mod for Minetest
 
-Repo for Currency Mod
+Adds a series of banknotes and universal basic income that will give them to
+players each period if they have broken or placed nodes.
+
+Adds the exchange shop, a chest which can handle trades of up to six items from
+both the buyer's side and the seller's side.
+
+Adds the barter table, a table for allow players to trade in a safe fashion
+instead of dropping items on the ground. The barter table has an expiry period
+if a trade is not completed for a long time, after which it will try to return
+the items to the players; the period is configurable.
 
 # Settings
-
-Settings with default values:
-```
-# After how much idle-time barter table is reset (seconds)
-barter.chest.expireafter				15 * 60
-```
+See `settingtypes.txt`
--- craftitems.lua
+++ craftitems.lua
@@ -1,49 +1,49 @@
 local S = minetest.get_translator("currency")
 
 minetest.register_craftitem("currency:minegeld", {
-	description = S("@1 Note", "$1"),
+	description = S("$@1 Note", "1"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,0",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_2", {
-	description = S("@1 Note", "$2"),
+	description = S("$@1 Note", "2"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,1",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_5", {
-	description = S("@1 Note", "$5"),
+	description = S("$@1 Note", "5"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,2",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_10", {
-	description = S("@1 Note", "$10"),
+	description = S("$@1 Note", "10"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,3",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_20", {
-	description = S("@1 Note", "$20"),
+	description = S("$@1 Note", "20"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,4",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_50", {
-	description = S("@1 Note", "$50"),
+	description = S("$@1 Note", "50"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,5",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_100", {
-	description = S("@1 Note", "$100"),
+	description = S("$@1 Note", "100"),
 	inventory_image = "aussie_banknotes.png^[sheet:1x7:0,6",
 		stack_max = 65535,
 		groups = {minegeld = 1, minegeld_note = 1}
